472fae2f4aba303a03b68ca6a11adfb257260661
Leigh Simpson <code@simpleigh.com>
2014-01-27 12:16:33 +0000

    Added a simple login page to demonstrate SQLi

---
+++ sqli-login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SQL Injection</title>
+	</head>
+	<body>
+		<h1>SQL Injection</h1>
+		<p>
+<?php
+
+mysql_connect('localhost', 'security', 'security');
+mysql_select_db('security');
+
+$query = "SELECT *
+          FROM   users
+          WHERE  username = '{$_GET['username']}'
+          AND    password = '{$_GET['password']}'";
+$result = mysql_query($query) or exit(mysql_error());
+
+if (mysql_num_rows($result) > 0) {
+	echo 'You logged in!' . PHP_EOL;
+} else {
+	echo 'Login failed' . PHP_EOL;
+}
+
+mysql_free_result($result);
+mysql_close();
+?>
+			<a href="sqli-login.html">Back</a>
+		</p>
+	</body>
+</html>
