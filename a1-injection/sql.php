f7cbb18741a684c87493fcb6b2c9c6f6aaaf49e9
Leigh Simpson <code@simpleigh.com>
2014-07-10 14:05:00 +0100

    Reformatted whitespace in SQLi examples

--- a1-injection/sql.php
+++ a1-injection/sql.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>SQL Injection</title>
-	</head>
-	<body>
-		<h1>SQL Injection</h1>
-		<p>
+  <head>
+    <title>SQL Injection</title>
+  </head>
+  <body>
+    <h1>SQL Injection</h1>
+    <p>
 <?php
 
 mysql_connect('localhost', 'security', 'security');
@@ -18,15 +18,17 @@ $query = "SELECT *
 $result = mysql_query($query) or exit(mysql_error());
 
 if (mysql_num_rows($result) > 0) {
-	echo 'You logged in!' . PHP_EOL;
+    echo 'You logged in!' . PHP_EOL;
 } else {
-	echo 'Login failed' . PHP_EOL;
+    echo 'Login failed' . PHP_EOL;
 }
 
 mysql_free_result($result);
 mysql_close();
 ?>
-			<a href="sql.html">Back</a>
-		</p>
-	</body>
+    </p>
+    <p>
+      <a href="sql.html">Back</a>
+    </p>
+  </body>
 </html>
