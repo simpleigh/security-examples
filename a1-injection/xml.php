deda14393d461c96f89bf6f7d589e5bb3a8dc8c3
Leigh Simpson <code@simpleigh.com>
2014-07-10 15:57:25 +0100

    Refactored whitespace for XXE injection examples

--- a1-injection/xml.php
+++ a1-injection/xml.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>XML Entity Injection</title>
-	</head>
-	<body>
-		<h1>XML Entity Injection</h1>
-		<p>Results:</p>
-		<pre>
+  <head>
+    <title>XML Entity Injection</title>
+  </head>
+  <body>
+    <h1>XML Entity Injection</h1>
+    <p>Results:</p>
+    <pre>
 <?php
 $document = new DOMDocument();
 $document->resolveExternals = true;
@@ -15,6 +15,6 @@ $document->substituteEntities = true;
 $document->loadXML($_POST['xml']);
 echo $document->saveXML();
 ?>
-		</pre>
-	</body>
+    </pre>
+  </body>
 </html>
