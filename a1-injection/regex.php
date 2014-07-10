39a9d5308857878f8a55037ab404409d28ed397d
Leigh Simpson <code@simpleigh.com>
2014-07-10 15:24:03 +0100

    Reformatted whitespace in regex injection examples

--- a1-injection/regex.php
+++ a1-injection/regex.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Regex Injection (1)</title>
-	</head>
-	<body>
-		<h1>Regex Injection (1)</h1>
-		<p>Result:</p>
-		<pre>
+  <head>
+    <title>Regex Injection (1)</title>
+  </head>
+  <body>
+    <h1>Regex Injection (1)</h1>
+    <p>Result:</p>
+    <pre>
 <?php echo preg_replace($_GET['pattern'], $_GET['replacement'], $_GET['subject']); ?>
-		</pre>
-	</body>
+    </pre>
+  </body>
 </html>
--- a1-injection/regex2.php
+++ a1-injection/regex2.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Regex Injection (2)</title>
-	</head>
-	<body>
-		<h1>Regex Injection (2)</h1>
-		<p>Result:</p>
-		<pre>
+  <head>
+    <title>Regex Injection (2)</title>
+  </head>
+  <body>
+    <h1>Regex Injection (2)</h1>
+    <p>Result:</p>
+    <pre>
 <?php echo preg_replace('/' . $_GET['pattern'] . '/', $_GET['replacement'], $_GET['subject']); ?>
-		</pre>
-	</body>
+    </pre>
+  </body>
 </html>
