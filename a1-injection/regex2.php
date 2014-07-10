fdfd74e884f2d70e94f67c042a7e9208319526a2
Leigh Simpson <code@simpleigh.com>
2014-07-10 15:12:08 +0100

    Moved regex injection code injection subdirectory

--- a1-injection/regex2.php
+++ a1-injection/regex2.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Regex Injection (1)</title>
+		<title>Regex Injection (2)</title>
 	</head>
 	<body>
-		<h1>Regex Injection (1)</h1>
+		<h1>Regex Injection (2)</h1>
 		<p>Result:</p>
 		<pre>
 <?php echo preg_replace('/' . $_GET['pattern'] . '/', $_GET['replacement'], $_GET['subject']); ?>
