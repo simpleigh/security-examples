de012a1b419271b01dd194c0adac1ff08006841a
Leigh Simpson <code@simpleigh.com>
2014-07-10 15:46:41 +0100

    Added demo of filesystem injection

---
+++ a1-injection/filesystem.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Filesystem Injection</title>
+  </head>
+  <body>
+    <h1>Filesystem Injection</h1>
+    <p>File contents:</p>
+    <p>
+      <pre>
+<?php echo file_get_contents('files/' . $_GET['file']); ?>
+      </pre>
+    </p>
+    <p><a href="filesystem.html">Back</a></p>
+    <p><a href="menu.html">Back to menu</a></p>
+  </body>
+</html>
