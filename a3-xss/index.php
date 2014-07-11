1ae78aebe0d8c8d87f9e4ed23f765d27a9c47d38
Leigh Simpson <code@simpleigh.com>
2014-07-11 09:49:17 +0100

    Added XSS example

---
+++ a3-xss/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Reflected XSS</title>
+  </head>
+  <body>
+    <h1>Reflected XSS</h1>
+<?php if (array_key_exists('data', $_GET)): ?>
+    <h2>You said:</h2>
+    <p><?php echo $_GET['data']; ?></p>
+<?php endif; ?>
+    <h2>Say something:</h2>
+    <form action="index.php" method="get">
+      <input type="text" name="data"/><input type="submit"/>
+    </form>
+    <h2>How to use</h2>
+    <p>
+      Enter something in the box above.
+      The application will echo it back.
+    </p>
+    <h3>Hacking in</h3>
+    <p>Enter the following:</p>
+    <p><pre>&lt;script&gt;alert("Pwned");&lt;/script&gt;</pre></p>
+    <p><a href="index.php">Reset</a></p>
+  </body>
+</html>
